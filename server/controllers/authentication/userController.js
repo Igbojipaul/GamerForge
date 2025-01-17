28a4d3c6f9aa59fd977227bc288a738a360ea965
Igboji Paul <chidiebubepaul241@gmail.com>
2025-01-17 14:03:05 +0100

    configured cookie requests

--- server/controllers/authentication/userController.js
+++ server/controllers/authentication/userController.js
@@ -78,7 +78,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      // sameSite: 'strict',
+      sameSite: "None", 
       maxAge: 50 * 60 * 1000
     });
 
